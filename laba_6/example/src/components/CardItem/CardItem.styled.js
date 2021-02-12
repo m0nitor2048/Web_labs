1f80128d0175490ee977a8005bb2fa8b8434dc95
John <m0nitor2048@gmail.com>
2021-02-12 15:31:17 +0200

    add lab 6-9

--- laba_6/example/src/components/CardItem/CardItem.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components_styles';
-
-export const Footer = styled.div`
-    display: flex;
-    margin-top: 20px;
-    justify-content: space-between;
-`
--- laba_6/example/src/constants/constants.js
+++
@@ -1,6 +0,0 @@
-// const GENERIC_PX_SIZE = 4;
-
-// export const sizes = {
-//     px4: `${GENERIC_PX_SIZE * 4 }px`,
-//     px16
-// }
